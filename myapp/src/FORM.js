0362da50b1c4f5e5a1d7c494297ab153e30e58f2
Anujay Surana <anujaysurana1@gmail.com>
2020-09-08 22:37:31 +0400

    Completed Homepage[Animation and Toggle Added]

--- myapp/src/FORM.js
+++ myapp/src/FORM.js
@@ -1,12 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import './FORM.css'
 import X from './Images/X.png'
 
 function  FORM(){
+    
+
+    var up = <form className="fields">
+
+    <label id = "fields_label" for = "email">Enter email</label>
+    <input id = "fields_input" type = "email" id = "email"></input>
+    <br/>
+    <br/>
+    <label id = "fields_label" for = "username">Choose username</label>
+    <input id = "fields_input" type = "text" id = "username"></input>
+    <br/>
+    <br/>
+    <label id = "fields_label" for = "password">Choose password</label>
+    <input id = "fields_input" type = "password" id = "password"></input>
+    <br/>
+    <br/>
+    <label id = "fields_label" for = "confirmpassword">Confirm password</label>
+    <input id = "fields_input" type = "password" id ="confirmpassword"></input>
+    <br/>
+    <br/>
+    <button id = "Letsgo" type = "submit">Let's go!</button>
+</form> ;
+
+
+    var toggler = <form className="fields">
+
+    <br/>
+    <br/>
+    <label id = "fields_label" for = "username">Choose username</label>
+    <input id = "fields_input" type = "text" id = "username"></input>
+    <br/>
+    <br/>
+    <label id = "fields_label" for = "password">Choose password</label>
+    <input id = "fields_input" type = "password" id = "password"></input>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <button id = "Letsgo" type = "submit">Let's go!</button>
+</form> ;
+
+
+
+
 
     function Close(){
         document.querySelector('.FORMS').style.display = 'none';
+        SignIn(up)
+    }
+    
+    const [SignUp , SignIn] = useState(up);
+
+    function Change_Toggle_SignIn(){
+        SignIn(toggler)
+    }
+
+    function Change_Toggle_SignUp(){
+        SignIn(up)
     }
 
     return(
@@ -18,30 +73,10 @@ function  FORM(){
             <div className="Form_Inputs">
 
                 <div className="Sign">
-                    <button id = "Sign_up">Sign up</button>
-                    <button id = "Sign_in">Sign in</button>
+                    <button id = "Sign_up" onClick = {Change_Toggle_SignUp}>Sign up</button>
+                    <button id = "Sign_in" onClick = {Change_Toggle_SignIn} >Sign in</button>
                 </div>
-
-            <form className="fields">
-
-                <label id = "fields_label" for = "email">Enter email</label>
-                <input id = "fields_input" type = "email" id = "email"></input>
-                <br/>
-                <br/>
-                <label id = "fields_label" for = "username">Choose username</label>
-                <input id = "fields_input" type = "text" id = "username"></input>
-                <br/>
-                <br/>
-                <label id = "fields_label" for = "password">Choose password</label>
-                <input id = "fields_input" type = "password" id = "password"></input>
-                <br/>
-                <br/>
-                <label id = "fields_label" for = "confirmpassword">Confirm password</label>
-                <input id = "fields_input" type = "password" id ="confirmpassword"></input>
-                <br/>
-                <br/>
-                <button id = "Letsgo" type = "submit">Let's go!</button>
-            </form>
+                    {SignUp}
             </div>
             
         </div>
